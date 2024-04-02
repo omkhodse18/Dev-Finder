8bb7c268a69f4610bc85e5b7c94fbd4f85f5e4a3
omkhodse18 <omkarkhodse.18@gmail.com>
2024-04-02 21:09:53 +0530

    Done

--- Dev-Finder/src/App.jsx
+++ Dev-Finder/src/App.jsx
@@ -4,12 +4,14 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import './App.css'
+import Loader from './components/Loader';
 
 function App() {
   const [username, setUsername] = useState("omkhodse18");
   const [inputValue, setInputValue] = useState("");
   const [loading, setLoading] = useState(false);
-  const [userData, setUserData] = useState("omkhodse18") 
+  const [userData, setUserData] = useState("omkhodse18");
+  const [darkMode, setDarkMode] = useState(false);
 
   const url = "https://api.github.com/users/"
  
@@ -60,12 +62,12 @@ function App() {
  
   return (
 
-    <div className='flex flex-col  items-center w-[100vw] h-[100vh] bg-[rgb(234, 234, 236)]'>
+    <div className={`flex flex-col  items-center w-[100vw] h-[100vh] ${darkMode ? 'bg-slate-950' : 'bg-[rgb(234, 234, 236)]'}`}>
 
       
-      <div className="app flex flex-col space-y-3 w-[724px]" id='app-main-div'>
+      <div className="app flex flex-col space-y-3 w-[724px] justify-center" id='app-main-div'>
         
-        <Navbar/>
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode}/>
 
         <div className='p-2 shadow-sm'>
           <div className='flex flex-row justify-between h-14 mt-7' id='searchbar-container'>
@@ -77,17 +79,21 @@ function App() {
               onChange={changeHandler}
               required
               onKeyDown={keyPressHandler}
-              className='border rounded-3xl px-4 py-2 mr-2 w-[70%] focus:outline-none border-black focus:border-black font-inter text-lg '
+              className={`${darkMode ? 'border-teal-700 bg-teal-100 focus:border-teal-900 placeholder:bg-teal-100 border' : 'border-black border focus:border-black'} rounded-3xl px-4 py-2 mr-2 w-[70%] focus:outline-none font-inter text-lg`}
             />
 
-            <button onClick={clickHandler} className='bg-black text-white px-4 py-2 w-[30%] rounded-3xl font-poppins text-lg font-medium shadow-gray-500 shadow-md hover:shadow-gray-700 transition-shadow' id='search-btn'>
+            <button onClick={clickHandler} className={`${darkMode ? 'bg-teal-700 text-white shadow-teal-800 hover:shadow-teal-700' : 'bg-black text-white shadow-gray-500 hover:shadow-gray-700'}  px-4 py-2 w-[30%] rounded-3xl font-poppins text-lg font-medium  shadow-md  transition-shadow`} id='search-btn'>
               SEARCH
             </button>
           </div>
         </div>
 
+        
+        <Card userData={userData} darkMode={darkMode} setDarkMode={setDarkMode}/>
+        
+      
 
-        <Card userData={userData}/>
+       
 
       </div>
     </div>
